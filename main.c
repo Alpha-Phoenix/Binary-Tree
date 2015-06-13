0a0e683b70f1f9b385f1a1830e5cd5d5fe3e8f79
Michael Douglas Pacheco <michael.douglas1995@hotmail.com>
2015-06-13 20:51:05 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,14 +4,14 @@
 
 int main(void) {
     BinaryTree *tree = BinaryTree_Create();
-    Cell a = {0, 50};
-    Cell b = {1, 0};
+    Cell a = {1, 50};
+    Cell b = {0, 0};
     Cell c = {2, 100};
     BinaryTree_Insert(tree, a);
     BinaryTree_Insert(tree, b);
     BinaryTree_Insert(tree, c);
     printf("%d", tree->root->left->cell.value);
-    BinaryTree_Destroy(tree);
+    BinaryTree_Destroy(&tree);
     return 0;
 }
 
