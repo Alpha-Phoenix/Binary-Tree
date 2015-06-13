f185f090031bf4aba8f56d4dfdc10d28d7bc926b
Michael Douglas Pacheco <michael.douglas1995@hotmail.com>
2015-06-13 20:15:20 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_tree.h"
+
+int main(void) {
+    BinaryTree *tree = BinaryTree_Create();
+    Cell a = {0, 50};
+    Cell b = {0, 0};
+    Cell c = {0, 100};
+    BinaryTree_Insert(tree, a);
+    BinaryTree_Insert(tree, b);
+    BinaryTree_Insert(tree, c);
+    printf("%d", tree->root->left->cell.value);
+    BinaryTree_Destroy(tree);
+    return 0;
+}
+
